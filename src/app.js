926a1023e831bdb0b256e4e73637b3f18f289092
Jackmc <jackmc1122@gmail.com>
2019-04-23 16:15:28 -0700

    Access the submit button and inputs with the dom.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
-console.log('hello there');
+const submitButton = document.getElementById('submit');
+const jackTravel = document.getElementById('jackTravel');
+const jackMusic = document.getElementById('jackTravel');
+const jackActivity = document.getElementById('jackTravel');
+
+console.log(submitButton, jackTravel, jackMusic, jackActivity);
