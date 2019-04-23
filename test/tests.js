4456f303b590d1219a363bdef791dc4e9eee068c
Jackmc <jackmc1122@gmail.com>
2019-04-23 16:31:09 -0700

    Add test for all entries are wrong.

--- test/tests.js
+++ test/tests.js
@@ -1,12 +1,21 @@
 const test = QUnit.test;
 
-test('time to test a function', assert => {
+function quizScore(travelAnswer, musicAnswer, activityAnswer) {
+  let score = 0;
+
+  return score;
+}
+
+test('All entries are wrong expecting 0', assert => {
   //Arrange
   // Set up your parameters and expectations
-
+  const travelAnswer = 'Cleveland, Ohio';
+  const musicAnswer = 'Taylor Swift';
+  const activityAnswer = 'running';
+  const expect = 0;
   //Act 
   // Call the function you're testing and set the result to a const
-
+  const score = quizScore(travelAnswer, musicAnswer, activityAnswer);
   //Assert
-  assert.equal(true, false);
+  assert.equal(score, expect);
 });
