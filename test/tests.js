77e7b567fb8b39843c0cbb71dde75fb9caea4225
Jackmc <jackmc1122@gmail.com>
2019-04-23 16:50:49 -0700

    Change the expected answers to not be capitalized and set the give answers to lower case.

--- test/tests.js
+++ test/tests.js
@@ -3,15 +3,19 @@ const test = QUnit.test;
 function quizScore(travelAnswer, musicAnswer, activityAnswer) {
   let score = 0;
 
-  if(travelAnswer === 'Leland, Michigan' || travelAnswer === 'Vail, Colorado' || 
-  travelAnswer === 'Munich, Germany' || travelAnswer === 'Innsbruck, Austria' || 
-  travelAnswer === 'Venice, Italy') {
+  travelAnswer = travelAnswer.toLowerCase();
+  musicAnswer = musicAnswer.toLowerCase();
+  activityAnswer = activityAnswer.toLowerCase();
+
+  if(travelAnswer === 'leland, michigan' || travelAnswer === 'vail, colorado' || 
+  travelAnswer === 'munich, germany' || travelAnswer === 'innsbruck, austria' || 
+  travelAnswer === 'venice, italy') {
     score++;
   }
 
-  if(musicAnswer === 'Muse' || musicAnswer === 'Mac Miller' || 
-  musicAnswer === 'Nick Hackim' || musicAnswer === 'Jinsang' || 
-  musicAnswer === 'The Beatles') {
+  if(musicAnswer === 'muse' || musicAnswer === 'mac miller' || 
+  musicAnswer === 'nick hackim' || musicAnswer === 'jinsang' || 
+  musicAnswer === 'the beatles') {
     score++;
   }
 
@@ -64,4 +68,19 @@ test('All entries are correct with different answers expecting 3', assert => {
   //Assert
   assert.equal(score, expect);
 });
+
+test('two entries are correct with bad capitalization expecting 2', assert => {
+  //Arrange
+  // Set up your parameters and expectations
+  const travelAnswer = 'vail, colorado';
+  const musicAnswer = 'jinsang';
+  const activityAnswer = 'reading';
+  const expect = 2;
+  //Act 
+  // Call the function you're testing and set the result to a const
+  const score = quizScore(travelAnswer, musicAnswer, activityAnswer);
+  //Assert
+  assert.equal(score, expect);
+});
+
 
