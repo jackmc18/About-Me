b291e782fdafb3bdff6294b5d7c7b1d2290632bf
Jackmc <jackmc1122@gmail.com>
2019-04-23 19:35:07 -0700

    Add labels/inputs and get the values in app.

--- src/app.js
+++ src/app.js
@@ -4,11 +4,15 @@ const submitButton = document.getElementById('submit');
 const jackTravelInput = document.getElementById('jackTravel');
 const jackMusicInput = document.getElementById('jackMusic');
 const jackActivityInput = document.getElementById('jackActivity');
+const jackSchoolInput = document.getElementById('jackSchool');
+const jackAgeInput = document.getElementById('jackAge');
 
 submitButton.addEventListener('click', () => {
   const travelAnswer = jackTravelInput.value;
   const musicAnswer = jackMusicInput.value;
   const activityAnswer = jackActivityInput.value;
+  const schoolAnswer = jackSchoolInput.value;
+  const ageAnswer = jackAgeInput.value;
 
   let score = 0;
   let scoreEl = document.getElementById('score');
